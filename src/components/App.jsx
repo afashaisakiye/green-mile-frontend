0fae75a4f90ace92a9c32d31c6ad5082262d36e2
Afasha Isakiye <afashaisakiye@gmail.com>
2020-03-02 15:17:32 +0300

    Finisshed packages page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import {BrowserRouter as Router,Switch,Route , useHistory } from "react-router-dom";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
 import PackagesPage from '../pages/PackagesPage';
 import Index from '../pages/Index';
+import CreateSuplier from './../pages/CreateSuplier';
 
 
 export default function App() {
@@ -21,6 +22,9 @@ export default function App() {
           </Route>
           <Route path="/dashboard">
             <Dashboard />
+          </Route>
+          <Route exact path="/supliers/add">
+            <CreateSuplier />
           </Route>
         </Switch>
     </Router>
