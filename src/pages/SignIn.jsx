005092ecf01694802fea959d12934336d8913caf
wavamuno <phoebemire@gmail.com>
2020-03-10 19:14:50 +0300

    changes

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,13 +3,16 @@ import "../signin.scss"
 
 export default function SignIn() {
     return (
-        <form action="action_page.php" method="post">
+        <form action="action_page.php" method="post" class="sign-in">
+            <div class="imgcontainer">
+                <img  class="img" src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png" alt="avatar" />
+            </div>
             <div class="container">
                 <label for="username"><br />Username</label>
                 <input type="text" placeholder="Enter Username" name="username" required />
                 <label For="password"><b>Password</b></label>
                 <input type="text" placeholder="Enter Password" required />
-                <button type="submit">SignIn</button>
+                <button type="submit">Continue</button>
 
             </div>
             <div class="container">
