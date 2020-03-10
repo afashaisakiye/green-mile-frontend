034197389efaf7cc8a30222e4aae119956c2f62c
wavamuno <phoebemire@gmail.com>
2020-03-10 15:07:10 +0300

    styling form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import "../signin.scss"
 
 export default function SignIn() {
     return (
-        <div>
-           <input type="text"/>
-        </div>
+        <form action="action_page.php" method="post">
+            <div class="container">
+                <label for="username"><br />Username</label>
+                <input type="text" placeholder="Enter Username" name="username" required />
+                <label For="password"><b>Password</b></label>
+                <input type="text" placeholder="Enter Password" required />
+                <button type="submit">SignIn</button>
+
+            </div>
+            <div class="container">
+                <button type="button" class="cancelbtn">Cancel</button>
+                <span class="passsword">Forgot <a href="#">password?</a></span>
+
+            </div>
+        </form>
     )
 }
