bc49e9a47412c58f5618c1d28810b95710e47d24
Afasha Isakiye <afashaisakiye@gmail.com>
2020-03-16 11:27:09 +0300

    Added add user button

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -7,6 +7,10 @@ function UsersPage() {
     return (
             <div className="g-col" >
             <div className="g-tool-bar">
+                <div className="right-tools">
+                    <button className="btn btn-success">Add Users</button>
+                    <input type="text" className="form-control inline" name="" id="" aria-describedby="helpId" placeholder="Search Users" />
+                </div>
                 <Logout />
             </div>
             <div className="table-container">
