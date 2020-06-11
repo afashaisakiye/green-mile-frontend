857310a34cbbaef121fba1e7f82162b9cfa3cfc2
Afasha Isakiye <afashaisakiye@gmail.com>
2020-06-12 00:26:08 +0300

    More mantainess in add user route

--- src/utils/ui.js
+++ src/utils/ui.js
@@ -1,22 +1,39 @@
-export const menu_items=[
-    {
-        icon_class_name:"fa fa-home",
-        link_to:"/"
-      },
-      {
-          icon_class_name:"fas fa-box-open",
-          link_to:"/packages"
-      },
-      {
-          icon_class_name:"fas fa-users",
-          link_to:"/users"
-      },
-      {
-          icon_class_name:"fas fa-building",
-          link_to:"/hubs"
-      },
-      {
-        icon_class_name:"fas fa-credit-card",
-        link_to:"/payment"
-      }
-]
+export const menu_items = [
+  {
+    icon_class_name: "fa fa-home",
+    link_to: "/",
+  },
+  {
+    icon_class_name: "fas fa-box-open",
+    link_to: "/packages",
+  },
+  {
+    icon_class_name: "fas fa-users",
+    link_to: "/users",
+  },
+  {
+    icon_class_name: "fas fa-building",
+    link_to: "/hubs",
+  },
+  {
+    icon_class_name: "fas fa-credit-card",
+    link_to: "/payment",
+  },
+];
+
+export const addUserFeilds = {
+  first_name: {
+    half: true,
+    placeholder: "First Name",
+    label: "First Name",
+    type: "text",
+    optional: false,
+  },
+  last_name: {
+    half: true,
+    placeholder: "Last Name",
+    label: "Last Name",
+    type: "text",
+    optional: false,
+  },
+};
