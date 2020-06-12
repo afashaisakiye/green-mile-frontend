d34248f0662a4d1e4a336939908228a8cc4e7836
Afasha Isakiye <afashaisakiye@gmail.com>
2020-06-12 06:12:07 +0300

    Code refactor to remove duplicates

---
+++ src/components/cores/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Button = () => {
+  return (
+    <button className="btn btn-primary btn-lg btn-block" type="submit">
+      Submit User
+    </button>
+  );
+};
