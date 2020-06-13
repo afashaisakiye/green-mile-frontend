dd091c91637f1134bb6d0dff05c10bf1b9ebc3f2
Afasha Isakiye <afashaisakiye@gmail.com>
2020-06-13 22:36:12 +0300

    Now can save login details

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,10 @@
 import React, { createContext, useState, useEffect } from "react";
 import { accountTypeInfo, signInUser } from "./../utils/api";
+import { getLocalStorage,setLocalStorage} from "./../utils/func"
 export const AuthContext = createContext();
 
 function AuthContextProvider(props) {
-  const [authed, setAuth] = useState(false);
+  const [authed, setAuth] = useState(getLocalStorage('auth'));
   const [account_type_info, setAccountTypeInfo] = useState(false);
 
   const signin = async (email, password) => {
@@ -11,6 +12,7 @@ function AuthContextProvider(props) {
     if(loggin.error_msg){
       return loggin;
     }
+    setLocalStorage('auth',loggin)
     setAuth(loggin);
     //do all the preparation here
     return true;
