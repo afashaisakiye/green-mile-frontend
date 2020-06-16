e38bb076d1f1a6a217cf86eff5781535489a347d
Afasha Isakiye <afashaisakiye@gmail.com>
2020-06-16 21:12:38 +0300

    Modified private routing to be able to check multiple conditions

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
-const PrivateRoute = ({ test,redirect_to, component: Component, children, ...rest }) => {
-  const component=(children && !Component)?children:<Component {...rest} />
-  return (
-      <Route {...rest} render={ props => ( test? component: <Redirect to={{ pathname: redirect_to, state: { from: props.location } }} />) }/>
-  )
+const PrivateRoute = ({ tests, component: Component, children, ...rest }) => {
+  const component = children && !Component ? children : <Component {...rest} />;
+  if (tests) {
+    const first_false = tests.filter((test) => test.test == false)[0];
+    if (first_false) {
+      if (first_false.redirect != undefined) {
+        return (
+          <Route
+            {...rest}
+            render={(props) => (
+              <Redirect
+                to={{
+                  pathname: first_false.redirect,
+                  state: { from: props.location },
+                }}
+              />
+            )}
+          />
+        );
+      } else if (first_false.component) {
+        return <first_false.component />;
+      }
+    }
+  }
+
+  return <Route {...rest} render={(props) => component} />;
 };
 
 export default PrivateRoute;
