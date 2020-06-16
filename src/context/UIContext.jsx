fa45ea724577831c54e2a1500177c1d1b8906a5b
Afasha Isakiye <afashaisakiye@gmail.com>
2020-06-16 21:11:35 +0300

    Can provide accessible pages data

--- src/context/UIContext.jsx
+++ src/context/UIContext.jsx
@@ -6,7 +6,6 @@ export const UIContext = createContext();
 function UIContextProvider(props) {
   const [menu_items,set_menu_items] =useState([]);
   const { account_type_info } = useContext(AuthContext);
-
   useEffect(() => {
     if(account_type_info){
       set_menu_items(all_menu_items.filter((menu_item)=>{
@@ -14,8 +13,9 @@ function UIContextProvider(props) {
       }))
     }
   }, [account_type_info])
+
     return (
-      <UIContext.Provider value={{sidemenu:{menu_items} }} >
+      <UIContext.Provider value={{sidemenu:{menu_items},accessible_pages:account_type_info.page_access_level }} >
         {props.children}
       </UIContext.Provider>
     );
