d9869730d0e8c1d8a7fc3676432000fdc7a09485
Afasha Isakiye <afashaisakiye@gmail.com>
2020-06-16 11:17:45 +0300

    Created Dummy NofAuthoried.jsx

---
+++ src/pages/NotAuthorised.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotAuthorised = () => {
+  return(
+    <div className="g-col no-sumary-card">
+        <h1>401- You can't see this page.</h1>
+    </div>
+  )
+}
+
+export default NotAuthorised;
