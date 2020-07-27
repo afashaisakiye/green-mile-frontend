7dda62c51eb3c210a5540fb882a3e0b829672360
Isakiye Afasha <afashaisakiye@gmail.com>
2020-07-27 07:27:44 +0300

    fixed tests

--- src/components/UsersList/UserList.test.js
+++ src/components/UsersList/UserList.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import UserList from './UserList';
+import UsersList from './UsersList';
 
 test('loads and displays greeting', async () => {
     expect(true).toBeTruthy()
