3a1720a985f50ec95c1f178dd577273a5751a1f5
Isakiye Afasha <afashaisakiye@gmail.com>
2020-07-28 21:09:46 +0300

    Added logout pages

---
+++ src/pages/Logout/Logout.jsx
@@ -0,0 +1,17 @@
+import React , { useContext } from 'react'
+
+import { AuthContext } from './../../context/AuthContext'
+
+const Logout = (props) => {
+   const { logout} = useContext(AuthContext)
+   console.log(logout)
+    return (
+        <div>
+            <p>Are you sure you want to logout ?</p>
+            <a href="#">Cancel</a>
+            <button onClick={()=>logout()} className="btn btn-primary" >Sure</button>
+        </div>
+    )
+}
+
+export default Logout
