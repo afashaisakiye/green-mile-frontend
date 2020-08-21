7441e805375835f3b34b8b4ee57423e75c75d57d
Isakiye Afasha <afashaisakiye@gmail>
2020-08-21 13:33:55 +0300

    Moved files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./signin.scss"
 import "./styles.scss";
 import GeneralContexts from "./context/GeneralContexts";
 let mountNode = document.querySelector("#app");
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import {Link } from "react-router-dom";
+import logo from './../../../public/img/logo.png'
+
 
 const LandingPage = () => {
     return (
         <div>
-            <div>
-                <h2>Welcome to green mile</h2>
-                <a href="/sign-in">Go to Login Page</a>
+            <div className="hero-conatainer">
+                <img className="img" src={logo} alt="avatar" />
+                <h2>Welcome to green mile, We shall transport your customer's packages gladly.</h2>
+                <Link  className="login-button" to="/sign-in">GO TO LOGIN PAGE</Link>
             </div>
         </div>
     )
